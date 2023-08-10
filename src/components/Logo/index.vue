a68c039a417d19962b8786b1840aefbaa1129660
kazimics <kazimicscn@gmail.com>
2023-08-11 00:16:52 +0800

    fix: 🐛 Fixing image file name error

--- src/components/Logo/index.vue
+++ src/components/Logo/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center h-[70px]">
     <img src="~assets/images/logo.png" alt="Logo" class="w-16" />
-    <img src="~assets/images/tetx-logo.png" alt="Logo" class="w-32" />
+    <img src="~assets/images/text-logo.png" alt="Logo" class="w-32" />
   </div>
 </template>
 
